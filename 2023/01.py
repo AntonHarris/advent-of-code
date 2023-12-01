49da68c5dd18fed62536f1b9c049274d084b914f
AntonHarris <fourmi_007@hotmail.com>
2023-12-01 16:46:10 -0500

    Adding part 2, cleaning code that fetches first and last in part 1

--- 2023/01.py
+++ 2023/01.py
@@ -12,16 +12,28 @@ if __name__ == "__main__":
     data = infile.readlines()
     infile.close()
 
+    digits = {
+        "one":1,    "1":1,
+        "two":2,    "2":2,
+        "three":3,  "3":3,
+        "four":4,   "4":4,
+        "five":5,   "5":5,
+        "six":6,    "6":6,
+        "seven":7,  "7":7,
+        "eight":8,  "8":8,
+        "nine":9,   "9":9
+    }
+
     sum_part_one = 0
     for line in data:
         line = line.strip()
-        first = re.search("\d", line)
-        # TODO : improve to get last digit in one step
-        last = re.search("(\d)[^\d]*$", line)
-        last = re.search("\d", last.group())
-        sum_part_one = sum_part_one + int(first.group() + last.group())
+        matches = re.findall("\d", line)
+        sum_part_one = sum_part_one + digits[matches[0]]*10 + digits[matches[-1]]
     print("Part 1 sum: ", sum_part_one)
     
-    # TODO : complete part 2
+    sum_part_two = 0
     for line in data:
         line = line.strip()
+        matches = re.findall("(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)
+        sum_part_two = sum_part_two + digits[matches[0]]*10 + digits[matches[-1]]
+    print("Part 2 sum: ", sum_part_two)

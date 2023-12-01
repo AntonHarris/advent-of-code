cd96fa836fda99f5f374994087e91aae4db6fb7c
AntonHarris <fourmi_007@hotmail.com>
2023-12-01 02:08:00 -0500

    Adding TODOs for improvement + note to do part 2

--- 2023/01.py
+++ 2023/01.py
@@ -2,17 +2,24 @@
 
 import re
 
+# TODO : file passed as argument to script
 infile = open("./input_files/01_data.dat", "r")
 data = infile.readlines()
 infile.close()
 
-sum = 0
+sum_part_one = 0
 for line in data:
     line = line.strip()
     first = re.search("\d", line)
+    # TODO : improve to get last digit in one step
     last = re.search("(\d)[^\d]*$", line)
     last = re.search("\d", last.group())
-    sum = sum + int(first.group() + last.group())
+    sum_part_one = sum_part_one + int(first.group() + last.group())
 
+# TODO : print in one line
 print("Part 1 sum: ")
-print(sum)
+print(sum_part_one)
+
+# TODO : complete part 2
+for line in data:
+    line = line.strip()

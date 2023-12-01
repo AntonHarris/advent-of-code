975da88cdaa1d45c2767c190fe807192248966d4
AntonHarris <fourmi_007@hotmail.com>
2023-12-01 02:04:39 -0500

    Advent of Code 2023 in Python, problem 1, part 1

--- 2023/01.py
+++ 2023/01.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+import re
+
+infile = open("./input_files/01_data.dat", "r")
+data = infile.readlines()
+infile.close()
+
+sum = 0
+for line in data:
+    line = line.strip()
+    first = re.search("\d", line)
+    last = re.search("(\d)[^\d]*$", line)
+    last = re.search("\d", last.group())
+    sum = sum + int(first.group() + last.group())
+
+print("Part 1 sum: ")
+print(sum)

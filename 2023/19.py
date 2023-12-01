af80cc056bab6009cbc58f8cc0df37a913e30b27
AntonHarris <fourmi_007@hotmail.com>
2023-12-01 02:21:16 -0500

    Correcting script name in usage message

--- 2023/19.py
+++ 2023/19.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     if (len(sys.argv)<2):
-        print("Usage: 01.py file_to_use")
+        print("Usage: 19.py file_to_use")
         exit(1)
 
     infile = open(sys.argv[1], "r")

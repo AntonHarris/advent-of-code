278f9de5a7788091abc063dd0dc6956585ecb12c
AntonHarris <fourmi_007@hotmail.com>
2023-12-01 02:16:20 -0500

    File as argument, print in one line

--- 2023/01.py
+++ 2023/01.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 
+import sys
 import re
 
-# TODO : file passed as argument to script
-infile = open("./input_files/01_data.dat", "r")
-data = infile.readlines()
-infile.close()
+if __name__ == "__main__":
+    if (len(sys.argv)<2):
+        print("Usage: 01.py file_to_use")
+        exit(1)
 
-sum_part_one = 0
-for line in data:
-    line = line.strip()
-    first = re.search("\d", line)
-    # TODO : improve to get last digit in one step
-    last = re.search("(\d)[^\d]*$", line)
-    last = re.search("\d", last.group())
-    sum_part_one = sum_part_one + int(first.group() + last.group())
+    infile = open(sys.argv[1], "r")
+    data = infile.readlines()
+    infile.close()
 
-# TODO : print in one line
-print("Part 1 sum: ")
-print(sum_part_one)
-
-# TODO : complete part 2
-for line in data:
-    line = line.strip()
+    sum_part_one = 0
+    for line in data:
+        line = line.strip()
+        first = re.search("\d", line)
+        # TODO : improve to get last digit in one step
+        last = re.search("(\d)[^\d]*$", line)
+        last = re.search("\d", last.group())
+        sum_part_one = sum_part_one + int(first.group() + last.group())
+    print("Part 1 sum: ", sum_part_one)
+    
+    # TODO : complete part 2
+    for line in data:
+        line = line.strip()

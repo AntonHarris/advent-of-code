1fc01f16da06aa76bd9096a60dbed9d9c21aac47
AntonHarris <fourmi_007@hotmail.com>
2023-12-01 02:19:10 -0500

    Adding template for start of each script

--- 2023/18.py
+++ 2023/18.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    if (len(sys.argv)<2):
+        print("Usage: 01.py file_to_use")
+        exit(1)
+
+    infile = open(sys.argv[1], "r")
+    data = infile.readlines()
+    infile.close()

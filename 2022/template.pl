597b9d32522b13326c05edb2a3971f2b53d8d3e7
AntonHarris <fourmi_007@hotmail.com>
2023-12-05 01:26:29 -0500

    Improve Perl template for AoC 2022

--- 2022/template.pl
+++ 2022/template.pl
@@ -3,13 +3,17 @@
 use strict;
 use warnings;
 
-use constant INPUT_FILE => "input_files/dayXX_input.txt";
+my $INPUT_FILE = $ARGV[0] if @ARGV>0 or die "No input file specified.\n";
 
 sub dayXX_1 {
 }
 
 sub dayXX_2 {
 }
+
+open my $fh, "< ", $INPUT_FILE or die "$INPUT_FILE: $!";
+chomp(my @data = <$fh>);
+close ($fh);
 
 dayXX_1();
 dayXX_2();

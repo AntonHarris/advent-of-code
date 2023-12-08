a3f1a10f9f4ee5fda9ad8cc283fdac0065430237
AntonHarris <fourmi_007@hotmail.com>
2023-12-08 00:54:53 -0500

    Day 08 part 1

--- 2023/08.py
+++ 2023/08.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import re
 
 if __name__ == "__main__":
     if (len(sys.argv)<2):
@@ -9,3 +10,20 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], 'r') as infile:
         data = [line.strip() for line in infile.readlines()]
+
+    directions = [*data[0]]
+    nodes = {}
+    for line in data[2:]:
+        node = line.split(" = ")[0]
+        children = re.sub(r'[(,)]', '', line.split(" = ")[1])
+        nodes[node] = {"L": children.split(" ")[0], "R": children.split(" ")[1]}
+
+    count_steps = 0
+    current_node = "AAA"
+    direction_idx = 0
+    while current_node != "ZZZ":
+        current_node = nodes[current_node][directions[direction_idx]]
+        count_steps += 1
+        direction_idx += 1
+        direction_idx = direction_idx%(len(directions))
+    print("Number of steps: ", count_steps)

7553b335cf667369b24ca1b1206322eee90017fd
AntonHarris <fourmi_007@hotmail.com>
2023-12-09 01:28:21 -0500

    AoC 2023 day 09 parts 1 and 2

--- 2023/09.py
+++ 2023/09.py
@@ -2,6 +2,10 @@
 
 import sys
 
+# stolen from Qiqi-impact
+def ints(line: str, split=' '):
+    return [int(x) for x in line.split(split) if x]
+
 if __name__ == "__main__":
     if (len(sys.argv)<2):
         print("Usage: 09.py file_to_use")
@@ -9,3 +13,21 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], 'r') as infile:
         data = [line.strip() for line in infile.readlines()]
+
+    sum_ends = 0
+    sum_beginnings = 0
+    for line in data:
+        sequences = []
+        sequences.append(ints(line))
+        while sum(sequences[-1])!=0 or (sum(sequences[-1])==0 and len(set(sequences[-1]))!=1):
+            tmp_arr = []
+            for i in range(0, len(sequences[-1])-1):
+                tmp_arr.append(sequences[-1][i+1] - sequences[-1][i])
+            sequences.append(tmp_arr)
+        for i in range(len(sequences)-2, -1, -1):
+            sequences[i].append(sequences[i][-1] + sequences[i+1][-1])
+            sequences[i].insert(0, sequences[i][0] - sequences[i+1][0])
+        sum_ends += sequences[0][-1]
+        sum_beginnings += sequences[0][0]
+    print("Sum ends: ",sum_ends)
+    print("Sum beginnings: ", sum_beginnings)

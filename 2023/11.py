9a0d86466e134a5d9261725b2690f4eb2288e78f
AntonHarris <fourmi_007@hotmail.com>
2023-12-11 16:54:24 -0500

    AoC day 11 part 1 start-slash-prep

--- 2023/11.py
+++ 2023/11.py
@@ -2,6 +2,14 @@
 
 import sys
 
+def is_row_empty(line) -> bool:
+    if len(set(line))==1 and line[0]==".":
+        return True
+    return False
+
+def is_col_empty(col, data) -> bool:
+    pass
+
 if __name__ == "__main__":
     if (len(sys.argv)<2):
         print("Usage: 11.py file_to_use")
@@ -9,3 +17,6 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], 'r') as infile:
         data = [line.strip() for line in infile.readlines()]
+    
+    for line in data:
+        print("line: ", line, " - is it empty? ", is_row_empty(line))

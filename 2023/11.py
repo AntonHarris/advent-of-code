2e8c295fdb22d8871645ce4f0ab4b9ed6fd1664c
AntonHarris <fourmi_007@hotmail.com>
2023-12-12 01:31:28 -0500

    AoC 2023 day 11 part 1 + data files for day 12

--- 2023/11.py
+++ 2023/11.py
@@ -2,13 +2,10 @@
 
 import sys
 
-def is_row_empty(line) -> bool:
+def is_line_empty(line) -> bool:
     if len(set(line))==1 and line[0]==".":
         return True
     return False
-
-def is_col_empty(col, data) -> bool:
-    pass
 
 if __name__ == "__main__":
     if (len(sys.argv)<2):
@@ -17,6 +14,35 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], 'r') as infile:
         data = [line.strip() for line in infile.readlines()]
-    
-    for line in data:
-        print("line: ", line, " - is it empty? ", is_row_empty(line))
+    sky_map = [[*line] for line in data]
+
+    # Add extra rows for "empty" lines
+    empty_rows = []
+    for idx, line in enumerate(sky_map):
+        if is_line_empty(line):
+            empty_rows.insert(0, idx)
+    for idx in empty_rows:
+        sky_map.insert(idx, ['.'] * len(sky_map[0]))
+    # Add extra columns for "empty" columns
+    empty_cols = []
+    for j in range(0, len(sky_map[0])):
+        tmp_line = []
+        for i in range(0, len(sky_map)):
+            tmp_line.append(sky_map[i][j])
+        if is_line_empty(tmp_line):
+            empty_cols.insert(0, j)
+    for idx in empty_cols:
+        for i in range(0, len(sky_map)):
+            sky_map[i].insert(idx, '.')
+    # Create vector/array of different galaxies
+    galaxies = []
+    for row in range(0, len(sky_map)):
+        for col in range(0, len(sky_map[row])):
+            if sky_map[row][col] == '#':
+                galaxies.append((row, col))
+    # Calculate differences for all sets of galaxies
+    sum_paths = 0
+    for i in range(0, len(galaxies)-1):
+        for j in range(i+1, len(galaxies)):
+            sum_paths += ((galaxies[j][0] - galaxies[i][0]) + (abs(galaxies[j][1] - galaxies[i][1])))
+    print("Sum paths between galaxies after expansion: ", sum_paths)

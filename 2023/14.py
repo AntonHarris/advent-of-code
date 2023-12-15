0d0815036416bc8a4a6f2a1465c564324a9d26f2
AntonHarris <fourmi_007@hotmail.com>
2023-12-15 01:34:50 -0500

    AoC 2023 day 14 part 1, test and data files for days 14 and 15

--- 2023/14.py
+++ 2023/14.py
@@ -2,6 +2,23 @@
 
 import sys
 
+def tilt_north(dish_mapping):
+    for j in range(len(dish_mapping[0])):
+        next_free = 0
+        for i in range(len(dish_mapping)):
+            if dish_mapping[i][j]=='#':
+                next_free = i+1
+            elif dish_mapping[i][j]=='O':
+                dish_mapping[i][j]='.'
+                dish_mapping[next_free][j]='O'
+                next_free += 1
+            else:
+                pass
+    return dish_mapping
+
+def count_rocks(line) -> int:
+    return line.count('O')
+
 if __name__ == "__main__":
     if (len(sys.argv)<2):
         print("Usage: 14.py file_to_use")
@@ -9,3 +26,10 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], 'r') as infile:
         data = [line.strip() for line in infile.readlines()]
+    dish_mapping = [[*line] for line in data]
+
+    dish_mapping = tilt_north(dish_mapping)
+    sum_rock_vals = 0;
+    for i in range(len(dish_mapping)):
+        sum_rock_vals = sum_rock_vals + (len(dish_mapping)-i)*count_rocks(dish_mapping[i])
+    print("Sum of rock values: ", sum_rock_vals)

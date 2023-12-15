b25fba0037fc79ce8003da98b5ee07e07e586802
AntonHarris <fourmi_007@hotmail.com>
2023-12-15 01:45:04 -0500

    AoC 2023 day 15 part 1

--- 2023/15.py
+++ 2023/15.py
@@ -9,3 +9,14 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], 'r') as infile:
         data = [line.strip() for line in infile.readlines()]
+    hashes = data[0].split(',')
+
+    sum_hashes = 0
+    for new_hash in hashes:
+        curr_hash = 0
+        for char in new_hash:
+            curr_hash += ord(char)
+            curr_hash *= 17
+            curr_hash %= 256
+        sum_hashes += curr_hash
+    print(f"Sum hashes = {sum_hashes}")

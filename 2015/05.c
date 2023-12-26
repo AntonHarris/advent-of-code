6be4d89bbba0ed5c0fd451d237f92643e5bba8d8
AntonHarris <fourmi_007@hotmail.com>
2023-12-26 03:45:44 -0500

    AoC 2015 day 05, parts 1 and 2

--- 2015/05.c
+++ 2015/05.c
@@ -1,12 +1,89 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 100
 
 char * chomp(char *p) {
     while (p[strlen(p)-1] == '\n') {
         p[strlen(p)-1] = '\0';
     }
     return p;
+}
+
+bool is_vowel (char c) {
+    switch (tolower(c)) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            return true;
+    }
+    return false;
+}
+
+bool has_three_vowels (const char *s) {
+    char *ptr_s = (char *)s;
+    unsigned int count_vowels = 0;
+    while (*ptr_s) {
+        if (is_vowel(*ptr_s)) {
+            count_vowels++;
+            if (count_vowels>=3) {
+                return true;
+            }
+        }
+        ptr_s++;
+    }
+
+    return false;
+}
+
+bool letter_repeat_with_spacing (const char *s, unsigned int spacing) {
+    char *ptr_s = (char *)s+spacing;
+
+    while (*ptr_s) {
+        if (*ptr_s == *(ptr_s-spacing)) {
+            return true;
+        }
+        ptr_s++;
+    }
+
+    return false;
+}
+
+bool contain_forbidden_strings (const char *s) {
+    char *forbidden_strings[] = {"ab", "cd", "pq", "xy"};
+    size_t len_forbidden_strings = sizeof(forbidden_strings)/sizeof(forbidden_strings[0]);
+
+    for (size_t i=0 ; i<len_forbidden_strings ; i++) {
+        if (strstr(s, forbidden_strings[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool pair_letters_repeat (const char *s) {
+    char tofind[3] = {0};
+    size_t max_attempts = strlen(s);
+    char *ptr_s = (char *)s;
+    if (max_attempts<4) {
+        return false;
+    }
+
+    max_attempts -= 3;
+    for (size_t i=0 ; i<max_attempts ; i++, ptr_s++) {
+        strncpy(tofind, ptr_s, 2);
+        if (strstr(ptr_s+2, tofind)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 int main (int argc, char**argv) {
@@ -22,6 +99,22 @@ int main (int argc, char**argv) {
         perror("Unable to open file");
         return 1;
     }
+    char buffer[BUFFER_SIZE];
+    unsigned int count_nice_strings_part_1 = 0;
+    unsigned int count_nice_strings_part_2 = 0;
+    while (fgets(buffer, sizeof(buffer), in_file)) {
+        chomp(buffer);
+        if (has_three_vowels(buffer) && letter_repeat_with_spacing(buffer, 1) && !contain_forbidden_strings(buffer)) {
+            count_nice_strings_part_1++;
+        }
+        if (pair_letters_repeat(buffer) && letter_repeat_with_spacing(buffer, 2)) {
+            count_nice_strings_part_2++;
+        }
+    }
     fclose(in_file);
 
+    printf("Number of nice strings: %u.\n", count_nice_strings_part_1);
+    printf("Number of nice strings: %u.\n", count_nice_strings_part_2);
+    
+    return 0;
 }

e742b50c8809a78644331667537a400471a51838
AntonHarris <fourmi_007@hotmail.com>
2023-12-26 13:38:40 -0500

    AoC 2015 day 05, making changed to clean and improve code

--- 2015/05.c
+++ 2015/05.c
@@ -3,11 +3,52 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <libgen.h>
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 32
+#define NO_SPACING 1
+#define ONE_SPACING 2
+
+char * chomp(char *p);
+bool is_vowel (char c);
+bool has_three_vowels (const char *s);
+bool letter_repeat_with_spacing (const char *s, unsigned int spacing);
+bool contain_forbidden_strings (const char *s);
+bool pair_letters_repeat (const char *s);
+
+int main (int argc, char**argv) {
+    if (argc<2) {
+        printf("Usage: %s [input_file_path/]input_file\n", basename(argv[0]));
+        return 1;
+    }
+
+    FILE *in_file = fopen(argv[1], "r");
+    if (in_file == NULL) {
+        perror("Unable to open file");
+        return 1;
+    }
+    char buffer[BUFFER_SIZE];
+    unsigned int count_nice_strings_part_1 = 0;
+    unsigned int count_nice_strings_part_2 = 0;
+    while (fgets(buffer, sizeof(buffer), in_file)) {
+        chomp(buffer);
+        if (has_three_vowels(buffer) && letter_repeat_with_spacing(buffer, NO_SPACING) && !contain_forbidden_strings(buffer)) {
+            count_nice_strings_part_1++;
+        }
+        if (pair_letters_repeat(buffer) && letter_repeat_with_spacing(buffer, ONE_SPACING)) {
+            count_nice_strings_part_2++;
+        }
+    }
+    fclose(in_file);
+
+    printf("Number of nice strings, part 1: %u.\n", count_nice_strings_part_1);
+    printf("Number of nice strings, part 2: %u.\n", count_nice_strings_part_2);
+    
+    return 0;
+}
 
 char * chomp(char *p) {
-    while (p[strlen(p)-1] == '\n') {
+    while (strlen(p) > 0 && p[strlen(p)-1] == '\n') {
         p[strlen(p)-1] = '\0';
     }
     return p;
@@ -26,29 +67,28 @@ bool is_vowel (char c) {
 }
 
 bool has_three_vowels (const char *s) {
-    char *ptr_s = (char *)s;
     unsigned int count_vowels = 0;
-    while (*ptr_s) {
-        if (is_vowel(*ptr_s)) {
+    while (*s) {
+        if (is_vowel(*s)) {
             count_vowels++;
             if (count_vowels>=3) {
                 return true;
             }
         }
-        ptr_s++;
+        s++;
     }
 
     return false;
 }
 
 bool letter_repeat_with_spacing (const char *s, unsigned int spacing) {
-    char *ptr_s = (char *)s+spacing;
+    s += spacing;
 
-    while (*ptr_s) {
-        if (*ptr_s == *(ptr_s-spacing)) {
+    while (*s) {
+        if (*s == *(s-spacing)) {
             return true;
         }
-        ptr_s++;
+        s++;
     }
 
     return false;
@@ -67,54 +107,21 @@ bool contain_forbidden_strings (const char *s) {
     return false;
 }
 
+// Alternative method: hash table of all letter pairs, letter pair as key, value is count of occurences
 bool pair_letters_repeat (const char *s) {
     char tofind[3] = {0};
     size_t max_attempts = strlen(s);
-    char *ptr_s = (char *)s;
     if (max_attempts<4) {
         return false;
     }
 
     max_attempts -= 3;
-    for (size_t i=0 ; i<max_attempts ; i++, ptr_s++) {
-        strncpy(tofind, ptr_s, 2);
-        if (strstr(ptr_s+2, tofind)) {
+    for (size_t i=0 ; i<max_attempts ; i++, s++) {
+        strncpy(tofind, s, 2);
+        if (strstr(s+2, tofind)) {
             return true;
         }
     }
 
     return false;
-}
-
-int main (int argc, char**argv) {
-    if (argc<2) {
-        char *lastslash = strrchr(argv[0], '/');
-        if (lastslash == NULL) { lastslash = argv[0]; }
-        printf("Usage: %s [input_file_path/]input_file\n", lastslash+1);
-        return 1;
-    }
-
-    FILE *in_file = fopen(argv[1], "r");
-    if (in_file == NULL) {
-        perror("Unable to open file");
-        return 1;
-    }
-    char buffer[BUFFER_SIZE];
-    unsigned int count_nice_strings_part_1 = 0;
-    unsigned int count_nice_strings_part_2 = 0;
-    while (fgets(buffer, sizeof(buffer), in_file)) {
-        chomp(buffer);
-        if (has_three_vowels(buffer) && letter_repeat_with_spacing(buffer, 1) && !contain_forbidden_strings(buffer)) {
-            count_nice_strings_part_1++;
-        }
-        if (pair_letters_repeat(buffer) && letter_repeat_with_spacing(buffer, 2)) {
-            count_nice_strings_part_2++;
-        }
-    }
-    fclose(in_file);
-
-    printf("Number of nice strings: %u.\n", count_nice_strings_part_1);
-    printf("Number of nice strings: %u.\n", count_nice_strings_part_2);
-    
-    return 0;
 }

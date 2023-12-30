707be9f365a46adb943136d2d6dcadc3cc81d537
AntonHarris <fourmi_007@hotmail.com>
2023-12-30 13:02:10 -0500

    AoC 2022 day 08, minor change to group code

--- 2022/08.pl
+++ 2022/08.pl
@@ -3,21 +3,19 @@
 use strict;
 use warnings;
 
-my $INPUT_FILE = $ARGV[0] if @ARGV>0 or die "No input file specified.\n";
-
 sub day08_1 {
     my @data = @_;
     my $data_len = scalar(@data);
     my $element_len = scalar(@{$data[0]});
     my @seen_array = ();
-    for my $i (0..$data_len-1) {
+    foreach my $i (0..$data_len-1) {
         push(@seen_array, [(0)x($element_len-1)]);
     }
 
     # left to right, right to left
-    for my $row (0..$data_len-1) {
+    foreach my $row (0..$data_len-1) {
         my ($tallest_tree_left, $tallest_tree_right) = (-1, -1);
-        for my $col (0..$element_len-1) {
+        foreach my $col (0..$element_len-1) {
             if ($data[$row][$col] > $tallest_tree_left) {
                 $seen_array[$row][$col] = 1;
                 $tallest_tree_left = $data[$row][$col];
@@ -29,9 +27,9 @@ sub day08_1 {
         }
     }
     # top to bottom, bottom to top
-    for my $col (0..$element_len-1) {
+    foreach my $col (0..$element_len-1) {
         my ($tallest_tree_top, $tallest_tree_bottom) = (-1, -1);
-        for my $row (0..$data_len-1) {
+        foreach my $row (0..$data_len-1) {
             if ($data[$row][$col] > $tallest_tree_top) {
                 $seen_array[$row][$col] = 1;
                 $tallest_tree_top = $data[$row][$col];
@@ -44,7 +42,7 @@ sub day08_1 {
     }
     # Count seen from seen_array
     my $count_seen = 0;
-    for my $line (@seen_array) {
+    foreach my $line (@seen_array) {
         $count_seen += scalar(grep { $_ == 1 } @$line );
     }
     print "Number of trees seen: $count_seen.\n";
@@ -55,8 +53,8 @@ sub day08_2 {
     my $data_len = scalar(@data);
     my $element_len = scalar(@{$data[0]});
     my $top_scenic_score = 0;
-    for my $row (1..$data_len-2) {
-        for my $col (1..$element_len-2) {
+    foreach my $row (1..$data_len-2) {
+        foreach my $col (1..$element_len-2) {
             my ($top_score, $right_score, $bottom_score, $left_score, $scenic_score) = (0, 0, 0, 0, 0);
             my $curr_tree_height = $data[$row][$col];
             # top
@@ -86,8 +84,9 @@ sub day08_2 {
     print "Top scenic score: $top_scenic_score.\n";
 }
 
+my $INPUT_FILE = $ARGV[0] if @ARGV>0 or die "No input file specified.\n";
 my @data = ();
-open my $fh, "< ", $INPUT_FILE or die "$INPUT_FILE: $!";
+open my $fh, "< ", $INPUT_FILE or die "Unable to open $INPUT_FILE: $!";
 while (<$fh>) {
     chomp;
     push(@data, [split(//, $_)]);

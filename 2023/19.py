5df89afcda80359567e7e6cfa2bc3d6eaa7e68e3
AntonHarris <fourmi_007@hotmail.com>
2024-01-04 02:05:12 -0500

    AoC 2023 day 19, starting code

--- 2023/19.py
+++ 2023/19.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import re
 
 if __name__ == "__main__":
     if (len(sys.argv)<2):
@@ -9,3 +10,23 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], 'r') as infile:
         data = [line.strip() for line in infile.readlines()]
+    
+    conditions = {}
+    condition_regex = r'^([a-z]+){([^}]+)}$'
+    idx = 0
+    for line in data:
+        if line=="":
+            break
+        else:
+            idx += 1
+        matches = re.match(condition_regex, line)
+        conditions.update({matches[1]: matches[2].split(r',')})
+    print(conditions)
+
+    idx += 1
+    sum_ratings = 0
+    # loop on parts
+    for line in data[idx:]:
+        pass
+        # print(line)
+    print(f"Sum parts ratings: {sum_ratings}")

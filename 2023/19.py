6b127a4636d864d9c6e930cb1b67b40469924c0c
AntonHarris <fourmi_007@hotmail.com>
2024-01-04 13:47:10 -0500

    AoC 2023 day 19, part 1 completed

--- 2023/19.py
+++ 2023/19.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
         else:
             idx += 1
         matches = re.match(condition_regex, line)
-        conditions.update({matches[1]: matches[2].split(r',')})
+        conditions.update({matches[1]: matches[2]})
+#        conditions.update({matches[1]: matches[2].split(r',')})
 
     idx += 1
     sum_ratings = 0
@@ -28,9 +29,27 @@ if __name__ == "__main__":
     part_regex = r'^{([^}]+)}$'
     for line in data[idx:]:
         part = {}
+        next_workflow = "in"
         matches = re.match(part_regex, line)
         line_arrayed = matches[1].split(r',')
         for i in line_arrayed:
             part.update({i.split(r'=')[0]: i.split(r'=')[1]})
-        # TODO: loop through conditions, stop when A or R, adding part["x"] to sum_ratings if accepted
+        while True:
+            if next_workflow=="A":
+                sum_ratings += sum([int(x) for x in part.values()])
+                break
+            elif next_workflow=="R":
+                break
+            else:
+                this_conditions = conditions[next_workflow].split(",")
+                for this_condition in this_conditions:
+                    if re.search(r':', this_condition) is not None:
+                        matches = re.match(r'^([a-z])(.)(\d+):(.+)$', this_condition)
+                        if eval(f"{part[matches[1]]}{matches[2]}{matches[3]}"):
+                            next_workflow = matches[4]
+                            break
+                    else:
+                        next_workflow = this_condition
+                        break
+
     print(f"Sum parts ratings: {sum_ratings}")

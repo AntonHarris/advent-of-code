235ac487a375adea78a7e16aa9194ca4a705ba6a
AntonHarris <fourmi_007@hotmail.com>
2024-01-06 16:24:40 -0500

    AoC 2023 day 03, re-did part 1 with arrays and completed part 2

--- 2023/03.py
+++ 2023/03.py
@@ -13,18 +13,41 @@ if __name__ == "__main__":
     
     symbols_list = []
     gears_list = []
-    numbers_list = []
-    number_regex, symbols_regex, gear_symbol_regex = r'(\d+)', r'[^\.\d]', r'\*'
+    numbers_dict = dict()
+    number_regex, symbols_regex = r'(\d+)', r'[^\.\d]'
     for row, line in enumerate(data):
         matches = re.finditer(symbols_regex, line)
-        symbol_id, number_id = 1, 1
         for match in matches:
-            symbols_list.append({"id":f"{row}-{symbol_id}", "row": row, "col": match.start()})
+            symbols_list.append({"id":f"{row}-{match.start()}", "row": row, "col": match.start()})
             if match.group(0) == '*':
-                gears_list.append({"id":f"{row}-{symbol_id}", "row": row, "col": match.start()})
-            symbol_id += 1
+                gears_list.append({"id":f"{row}-{match.start()}", "row": row, "col": match.start()})
         matches = re.finditer(number_regex, line)
         for match in matches:
-            numbers_list.append({"id":f"{row}-{number_id}", "row": row, "col_start": match.start(), "col_end": match.end()-1, "number":int(match.group(0))})
-            number_id += 1
+            numbers_dict.update({f"{row}-{match.start()}":{"row": row, "col_start": match.start(), "col_end": match.end()-1, "number":int(match.group(0))}})
 
+    # Loop through all symbols, find the numbers next to them, sum them all
+    numbers_id_set = set()
+    for symbol in symbols_list:
+        for number_id in numbers_dict.keys():
+            if (numbers_dict[number_id]["row"] == symbol["row"]-1 and ((numbers_dict[number_id]["col_end"] <= symbol["col"]+1 and numbers_dict[number_id]["col_end"] >= symbol["col"]-1) or (numbers_dict[number_id]["col_start"] >= symbol["col"]-1 and numbers_dict[number_id]["col_start"] <= symbol["col"]+1))) or \
+               (numbers_dict[number_id]["row"] == symbol["row"] and (numbers_dict[number_id]["col_end"] == symbol["col"]-1 or numbers_dict[number_id]["col_start"] == symbol["col"]+1)) or \
+               (numbers_dict[number_id]["row"] == symbol["row"]+1 and ((numbers_dict[number_id]["col_end"] <= symbol["col"]+1 and numbers_dict[number_id]["col_end"] >= symbol["col"]-1) or (numbers_dict[number_id]["col_start"] >= symbol["col"]-1 and numbers_dict[number_id]["col_start"] <= symbol["col"]+1))):
+                numbers_id_set.add(number_id)
+    parts_numbers_sum = 0
+    for number_id in numbers_id_set:
+        parts_numbers_sum += numbers_dict[number_id]["number"]
+    print(f"Parts sum: {parts_numbers_sum}")
+
+    # Loop through all the gear symbols, find the adjacent numbers, and if there are only 2 sum the multiplications
+    gear_ratios_sum = 0
+    for gear in gears_list:
+        gear_numbers_id_set = set()
+        for number_id in numbers_dict.keys():
+            if (numbers_dict[number_id]["row"] == gear["row"]-1 and ((numbers_dict[number_id]["col_end"] <= gear["col"]+1 and numbers_dict[number_id]["col_end"] >= gear["col"]-1) or (numbers_dict[number_id]["col_start"] >= gear["col"]-1 and numbers_dict[number_id]["col_start"] <= gear["col"]+1))) or \
+               (numbers_dict[number_id]["row"] == gear["row"] and (numbers_dict[number_id]["col_end"] == gear["col"]-1 or numbers_dict[number_id]["col_start"] == gear["col"]+1)) or \
+               (numbers_dict[number_id]["row"] == gear["row"]+1 and ((numbers_dict[number_id]["col_end"] <= gear["col"]+1 and numbers_dict[number_id]["col_end"] >= gear["col"]-1) or (numbers_dict[number_id]["col_start"] >= gear["col"]-1 and numbers_dict[number_id]["col_start"] <= gear["col"]+1))):
+                gear_numbers_id_set.add(number_id)
+        if len(gear_numbers_id_set) == 2:
+            gear_ratios_sum += (numbers_dict[gear_numbers_id_set.pop()]["number"]*numbers_dict[gear_numbers_id_set.pop()]["number"])
+    print(f"Sum of gear rations: {gear_ratios_sum}")
+        

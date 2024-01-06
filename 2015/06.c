bcab319cbe07d6cfcfce86f5d62aba7d17cebc4f
AntonHarris <fourmi_007@hotmail.com>
2024-01-05 23:06:09 -0500

    AoC 2015 day 06, using enums instead of numbers for function calls for actions or brightness

--- 2015/06.c
+++ 2015/06.c
@@ -6,16 +6,25 @@
 #define BUFFER_SIZE 100
 #define GRID_ROWS 1000
 #define GRID_COLS 1000
-#define TURN_ON 1
-#define TURN_OFF 0
+
+typedef enum Action {
+    ACT_TURN_OFF = 0,
+    ACT_TURN_ON = 1,
+} ACTION;
+
+typedef enum Brightness {
+    TURN_OFF = -1,
+    TURN_ON = 1,
+    TOGGLE = 2,
+} BRIGHTNESS;
 
 char * chomp (char *p);
 // Part 1 functions declarations
-void turn_on_off (int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[], int action);
+void turn_on_off (int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[], ACTION action);
 void toggle_lights (int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[]);
 int count_lights_lit (const int arr[GRID_ROWS][GRID_COLS]);
 // Part 2 functions declarations
-void change_light_brightness(int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[], int amount);
+void change_light_brightness(int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[], BRIGHTNESS change);
 int calc_lights_brightness(const int arr[GRID_ROWS][GRID_COLS]);
 
 int main (int argc, char**argv) {
@@ -39,17 +48,17 @@ int main (int argc, char**argv) {
             case ' ': // toggle
                 sscanf(buffer, "toggle %d,%d through %d,%d", &start_coords[0], &start_coords[1], &end_coords[0], &end_coords[1]);
                 toggle_lights(light_grid_part_1, start_coords, end_coords);
-                change_light_brightness(light_grid_part_2, start_coords, end_coords, 2);
+                change_light_brightness(light_grid_part_2, start_coords, end_coords, TOGGLE);
                 break;
             case 'f': // turn off
                 sscanf(buffer, "turn off %d,%d through %d,%d", &start_coords[0], &start_coords[1], &end_coords[0], &end_coords[1]);
-                turn_on_off(light_grid_part_1, start_coords, end_coords, TURN_OFF);
-                change_light_brightness(light_grid_part_2, start_coords, end_coords, -1);
+                turn_on_off(light_grid_part_1, start_coords, end_coords, ACT_TURN_OFF);
+                change_light_brightness(light_grid_part_2, start_coords, end_coords, TURN_OFF);
                 break;
             case 'n': // turn on
                 sscanf(buffer, "turn on %d,%d through %d,%d", &start_coords[0], &start_coords[1], &end_coords[0], &end_coords[1]);
-                turn_on_off(light_grid_part_1, start_coords, end_coords, TURN_ON);
-                change_light_brightness(light_grid_part_2, start_coords, end_coords, 1);
+                turn_on_off(light_grid_part_1, start_coords, end_coords, ACT_TURN_ON);
+                change_light_brightness(light_grid_part_2, start_coords, end_coords, TURN_ON);
                 break;
         }
     }
@@ -69,7 +78,7 @@ char * chomp (char *p) {
 }
 
 // Part 1 functions
-void turn_on_off (int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[], int action) {
+void turn_on_off (int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[], ACTION action) {
     for (int i=start_coords[0] ; i<=end_coords[0] ; i++) {
         for (int j=start_coords[1] ; j<=end_coords[1] ; j++) {
             arr[i][j] = action;
@@ -98,10 +107,10 @@ int count_lights_lit(const int arr[GRID_ROWS][GRID_COLS]) {
 }
 
 // Part 2 functions
-void change_light_brightness(int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[], int amount) {
+void change_light_brightness(int arr[GRID_ROWS][GRID_COLS], int start_coords[], int end_coords[], BRIGHTNESS change) {
     for (int i=start_coords[0] ; i<=end_coords[0] ; i++) {
         for (int j=start_coords[1] ; j<=end_coords[1] ; j++) {
-            arr[i][j] += amount;
+            arr[i][j] += change;
             if (arr[i][j] < 0) {
                 arr[i][j] = 0;
             }

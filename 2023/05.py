03a067bfc269ae0c967243c0e8ca29698dd9115c
AntonHarris <fourmi_007@hotmail.com>
2024-01-12 18:19:42 -0500

    AoC 2023 day 05, cleaning code, removing prints

--- 2023/05.py
+++ 2023/05.py
@@ -4,7 +4,9 @@ import sys
 
 def find_value_in_map(value: int, mapping_array) -> int:
     for my_map in mapping_array:
-        if value >= my_map[0] and value <= my_map[1]:
+        if value < my_map[0]:
+            return value
+        if value <= my_map[1]:
             return (value + my_map[3])
     return value
 
@@ -26,6 +28,8 @@ if __name__ == "__main__":
             parts = list(map(int, line.split(" ")))
             maps[-1].append([parts[1], parts[1]+parts[2]-1, parts[0], parts[0]-parts[1]])
             # src_start, src_end, dest_start, action
+    for idx in range(len(maps)):
+        maps[-1].sort(key = lambda x: x[0])
 
     smallest_location = sys.maxsize
     for seed in seeds:
@@ -35,20 +39,19 @@ if __name__ == "__main__":
         smallest_location = value if value<smallest_location else smallest_location
     print("Smallest location: ", smallest_location)
 
-    """ TODO for part 2
-        - don't try and do the calculations for all the seeds in a range,
-          but instead find the one in the range that's the smallest, then
-          go up to the next new mapping in the single map dictionary/array.
     """
-    # smallest_location = sys.maxsize
-    # seeds_array = data[0].split(": ")[1].split(" ")
-    # seeds_with_ranges = [[int(seeds_array[i]), int(seeds_array[i + 1])] for i in range(0, len(seeds_array), 2)]
-    # for seed_range in seeds_with_ranges:
-    #     print("Part 2, new range")
-    #     for seed in range(seed_range[0], seed_range[0] + seed_range[1]):
-    #         value = int(seed)
-    #         for mapping in maps:
-    #             value = find_value_in_map(value, mapping)
-    #         smallest_location = value if value<smallest_location else smallest_location # not re-initialising because it's still a valid value
-    # print("Smallest location, part 2: ", smallest_location)
+    Brute-force method, I got the answer after about 25 mins on my computer using 'pypy'.
+    TODO: find optimised algorithm.
+    Misc. optimisation: multi-threading for each range
+    """
+    smallest_location = sys.maxsize
+    seeds_array = data[0].split(": ")[1].split(" ")
+    seeds_with_ranges = [[int(seeds_array[i]), int(seeds_array[i + 1])] for i in range(0, len(seeds_array), 2)]
+    for seed_range in seeds_with_ranges:
+        for seed in range(seed_range[0], seed_range[0] + seed_range[1]):
+            value = int(seed)
+            for mapping in maps:
+                value = find_value_in_map(value, mapping)
+            smallest_location = value if value<smallest_location else smallest_location # not re-initialising because it's still a valid value
+    print("Smallest location, part 2: ", smallest_location)
 

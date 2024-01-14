710f7776493d862e1e640e3d4052b17a8f9a5c0a
AntonHarris <fourmi_007@hotmail.com>
2024-01-14 06:05:56 -0500

    AoC 2023 day 07, completed part 2, cleaned code

--- 2023/07.py
+++ 2023/07.py
@@ -41,9 +41,27 @@ def calc_hand_type (hand):
             return Hand_type.ONE_PAIR
     return Hand_type.HIGH_CARD
 
-# TODO: for part 2
+# Brute_force, probably a better way to do it
 def calc_hand_type_with_joker (hand):
-    return Hand_type.HIGH_CARD
+    ret_hand_type = Hand_type.HIGH_CARD
+    for c in card_order_low_to_high_with_joker[1:]:
+        tmp_hand = hand.replace("J", str(c))
+        tmp_hand_type = calc_hand_type(tmp_hand)
+        if tmp_hand_type.value > ret_hand_type.value:
+            ret_hand_type = tmp_hand_type
+    return ret_hand_type
+
+def compare_hands (hand1, hand1_type, hand2, hand2_type, comparaison_string):
+    if hand1_type.value > hand2_type.value:
+        return 1
+    if hand1_type.value < hand2_type.value:
+        return -1
+    for i in range(len(hand1.get_hand())):
+        if comparaison_string.index(hand1.get_hand()[i]) > comparaison_string.index(hand2.get_hand()[i]):
+            return 1
+        elif comparaison_string.index(hand1.get_hand()[i]) < comparaison_string.index(hand2.get_hand()[i]):
+            return -1
+    return 0 # identical hand   
 
 # Class to represent a "poker" hand
 class Card_hand:
@@ -56,7 +74,7 @@ class Card_hand:
         self.hand_type_joker = calc_hand_type_with_joker(hand)
     
     def __str__ (self):
-        return f"Hand: {self.hand}, hand type: {str(self.hand_type.name)}, bid: {self.bid}"
+        return f"Hand: {self.hand}, hand type: {self.hand_type.name}, joker hand type: {self.hand_type_joker.name}, bid: {self.bid}"
     
     def get_hand (self):
         return self.hand
@@ -65,23 +83,11 @@ class Card_hand:
         return self.hand_type
     
     def get_hand_type_with_joker (self):
-        return self.hand_type
+        return self.hand_type_joker
     
     def get_bid (self):
         return self.bid
 
-    def compare (self, hand, comparaison_string):
-        if self.hand_type.value > hand.get_hand_type().value:
-            return -1
-        if self.hand_type.value < hand.get_hand_type().value:
-            return 1
-        for i in range(len(self.hand)):
-            if comparaison_string.index(self.hand[i]) > comparaison_string.index(hand.hand[i]):
-                return -1
-            elif comparaison_string.index(self.hand[i]) < comparaison_string.index(hand.hand[i]):
-                return 1
-        return 0 # identical hand
-    
 # Main
 if __name__ == "__main__":
     if (len(sys.argv)<2):
@@ -95,22 +101,28 @@ if __name__ == "__main__":
     hands_with_joker = []
     for line in data:
         new_hand = Card_hand(*line.split(" "))
+
         idx = len(hands)
-        idx_with_joker = len(hands_with_joker)
-        for i in range(len(hands)):
-            if hands[i].compare(new_hand, card_order_low_to_high) < 0:
-                idx = i
-                break
-        for i in range(len(hands_with_joker)):
-            if hands_with_joker[i].compare(new_hand, card_order_low_to_high_with_joker) < 0:
-                idx_with_joker = i
-                break
+        if idx>0:
+            for i in range(len(hands)):
+                if compare_hands(hands[i], hands[i].get_hand_type(), new_hand, new_hand.get_hand_type(), card_order_low_to_high) > 0:
+                    idx = i
+                    break
         hands.insert(idx, new_hand)
+
+        idx_with_joker = len(hands_with_joker)
+        if idx_with_joker>0:
+            for i in range(len(hands_with_joker)):
+                if compare_hands(hands_with_joker[i], hands_with_joker[i].get_hand_type_with_joker(), new_hand, new_hand.get_hand_type_with_joker(), card_order_low_to_high_with_joker) > 0:
+                    idx_with_joker = i
+                    break
         hands_with_joker.insert(idx_with_joker, new_hand)
+
     total_winnings = 0
     for idx, hand in enumerate(hands):
         total_winnings += (idx+1)*hand.get_bid()
     print("Total_winnings: ", total_winnings)
+
     total_winnings_with_jokers = 0
     for idx, hand in enumerate(hands_with_joker):
         total_winnings_with_jokers += (idx+1)*hand.get_bid()

e7b2b060e2596488de974185b588deecc7cbe983
AntonHarris <fourmi_007@hotmail.com>
2024-02-12 23:42:59 -0500

    AoC 2023 day 12, add code to be able to complete part 2 once the algorithm is improved

--- 2023/12.py
+++ 2023/12.py
@@ -17,23 +17,34 @@ def try_possible_arrangements(line) -> int:
     row_springs, row_conditions = line.split(" ")
     row_springs_arr = list(row_springs)
     count_unknown = row_springs.count('?')
+    count_known =  row_springs.count('#')
     sum_damaged = sum([int(group) for group in row_conditions.split(",")])
 
-    # TODO: improve this algo (which takes about 21 seconds on my computer)
+    """
+    TODO: improve this algo (which takes about 21 seconds on my computer), as it is not quick
+    enough to be able to complete part 2
+    """
     count_possible_arrangements = 0
     for i in range(2**count_unknown):
-        row_springs_arr = list(row_springs)
-        tmp_i = i
-        for j, c in enumerate(row_springs_arr):
-            if c=='?':
-                row_springs_arr[j] = "#" if tmp_i%2 == 1 else "."
-                tmp_i //= 2
-        tmp_str = ""
-        for c in row_springs_arr:
-            tmp_str += c
-        count_possible_arrangements += 1 if check_conditions(tmp_str, row_conditions) else 0
+        if int.bit_count(i) == (sum_damaged-count_known):
+            row_springs_arr = list(row_springs)
+            tmp_i = i
+            for j, c in enumerate(row_springs_arr):
+                if c=='?':
+                    row_springs_arr[j] = "#" if tmp_i%2 == 1 else "."
+                    tmp_i //= 2
+            tmp_str = ""
+            for c in row_springs_arr:
+                tmp_str += c
+            count_possible_arrangements += 1 if check_conditions(tmp_str, row_conditions) else 0
 
     return count_possible_arrangements
+
+def try_possible_arrangements_unfolded(line) -> int:
+    row_springs, row_conditions = line.split(" ")
+    row_springs_unfolded = '?'.join([row_springs] * 5)
+    row_conditions_unfolded = ','.join([row_conditions] * 5)
+    return try_possible_arrangements((' '.join([row_springs_unfolded, row_conditions_unfolded])))
 
 if __name__ == "__main__":
     if (len(sys.argv)<2):
@@ -44,3 +55,4 @@ if __name__ == "__main__":
         data = [line.strip() for line in infile.readlines()]
 
     print("Sum of possible arrangements: ", sum([try_possible_arrangements(line) for line in data]))
+    # print("Sum of possible arrangements for unfolded map: ", sum([try_possible_arrangements_unfolded(line) for line in data]))

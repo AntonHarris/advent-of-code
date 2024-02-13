d1c3a0404ee9c3da3f4fb92867c4e6711dd2617a
AntonHarris <fourmi_007@hotmail.com>
2024-02-12 22:54:04 -0500

    AoC 2023 day 12, completed part 1 with an unoptimised algorithm

--- 2023/12.py
+++ 2023/12.py
@@ -5,7 +5,7 @@ import re
 
 def check_conditions(row_springs, row_conditions) -> bool:
     springs = re.findall(r'#+', row_springs)
-    conditions = re.split(r',', row_condition_record)
+    conditions = re.split(r',', row_conditions)
     if len(springs) != len(conditions):
         return False
     for check in zip(springs, conditions):
@@ -15,15 +15,25 @@ def check_conditions(row_springs, row_conditions) -> bool:
 
 def try_possible_arrangements(line) -> int:
     row_springs, row_conditions = line.split(" ")
+    row_springs_arr = list(row_springs)
     count_unknown = row_springs.count('?')
     sum_damaged = sum([int(group) for group in row_conditions.split(",")])
-    """
-    TODO:
-    Create loop to replace the question marks in the row_springs with the appropriate
-    symbols, and then validate if the proposed new row in valid or not by using the
-    check_conditions function. Count and return the number of valid arrangements.
-    """
-    return 1
+
+    # TODO: improve this algo (which takes about 21 seconds on my computer)
+    count_possible_arrangements = 0
+    for i in range(2**count_unknown):
+        row_springs_arr = list(row_springs)
+        tmp_i = i
+        for j, c in enumerate(row_springs_arr):
+            if c=='?':
+                row_springs_arr[j] = "#" if tmp_i%2 == 1 else "."
+                tmp_i //= 2
+        tmp_str = ""
+        for c in row_springs_arr:
+            tmp_str += c
+        count_possible_arrangements += 1 if check_conditions(tmp_str, row_conditions) else 0
+
+    return count_possible_arrangements
 
 if __name__ == "__main__":
     if (len(sys.argv)<2):
@@ -33,8 +43,4 @@ if __name__ == "__main__":
     with open(sys.argv[1], 'r') as infile:
         data = [line.strip() for line in infile.readlines()]
 
-    # sum_poss_arrangements = 0
-    # for line in data:
-    #     sum_poss_arrangements += try_possible_arrangements(line)
-    # print("Sum of possible arrangements: ", sum_poss_arrangements)
     print("Sum of possible arrangements: ", sum([try_possible_arrangements(line) for line in data]))

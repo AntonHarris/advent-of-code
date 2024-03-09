c4535ae0ddf57399f78ed6ebe69a84c48f6bb22e
AntonHarris <fourmi_007@hotmail.com>
2024-03-08 21:03:55 -0500

    AoC 2015 day 07, parts 1 and 2 completed

--- 2015/07.c
+++ 2015/07.c
@@ -5,33 +5,50 @@
 #include <ctype.h>
 #include <libgen.h>
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 50
 #define LABEL_SIZE 10
 
-typedef enum instruction_types {
+typedef enum {
     i_assign = 0,
     i_and,
     i_or,
     i_lshift,
     i_rshift,
     i_not,
-} INSTRUCTION_TYPES;
+} instruction_types;
 
-typedef struct wire_t {
+typedef struct {
+    char target_label[LABEL_SIZE];
+    char left_label[LABEL_SIZE];
+    instruction_types i_type;
+    char right_label[LABEL_SIZE];
+    bool val_available;
+    unsigned short val;
+} instruction_t;
+
+typedef struct instruction_list_t {
+    instruction_t instruction;
+    struct instruction_list_t *next;
+} instruction_list_t;
+
+typedef struct {
     char label[LABEL_SIZE];
     unsigned short val;
-} WIRE_T;
+} wire_t;
 
 typedef struct wire_list_t {
-    WIRE_T wire;
+    wire_t wire;
     struct wire_list_t *next;
-} WIRE_LIST_T;
+} wire_list_t;
 
-char* chomp(char *p);
-void handle_instruction (FILE *in_file, WIRE_LIST_T **head);
-void add_wire_to_list (const char *s, unsigned short val, WIRE_LIST_T **head);
-WIRE_LIST_T* find_wire (const char *s, WIRE_LIST_T *head);
-unsigned short get_wire_val (const char *s, WIRE_LIST_T *head);
+char* chomp (char *p);
+size_t count_spaces (const char *s);
+instruction_list_t* add_new_instruction (instruction_list_t *l, const char *s);
+instruction_t* get_target_instruction (instruction_list_t *l, const char *s);
+void override_instruction (instruction_list_t *l, const char *s, instruction_types it, const char *ll, const char *rl);
+void clear_values (instruction_list_t *l);
+void empty_and_free_instruction_list (instruction_list_t *l);
+unsigned short get_value_of_target (const char *t, instruction_list_t *il);
 
 int main (int argc, char **argv) {
     if (argc<2) {
@@ -39,23 +56,31 @@ int main (int argc, char **argv) {
     }
 
     FILE *in_file = fopen(argv[1], "r");
-    WIRE_LIST_T *head = NULL;
+    char buffer[BUFFER_SIZE] = {0};
+    instruction_list_t *instructions = NULL;
     if (in_file == NULL) {
         fprintf(stderr, "Unable to open file %s.\n", argv[1]);
         perror("Error message opening file file");
         return 1;
     }
-    while (!feof(in_file)) {
-        handle_instruction(in_file, &head);
+    while (fgets(buffer, sizeof(buffer), in_file)) {
+        chomp(buffer);
+        instructions = add_new_instruction(instructions, buffer);
     }
     fclose(in_file);
 
-    WIRE_LIST_T *tmp_ptr_head = head;
-    while (tmp_ptr_head) {
-        tmp_ptr_head = tmp_ptr_head->next;
-    }
+    char *target = "a";
+    unsigned short target_value = get_value_of_target(target, instructions);
+    
+    printf("Part 1: Value of wire %s = %u.\n", target, target_value);
 
-    printf("Value of wire a = %u.\n", get_wire_val("a", head));
+    sprintf(buffer, "%u", target_value);
+    clear_values(instructions);
+    override_instruction(instructions, "b", i_assign, "", buffer);
+    target_value = get_value_of_target(target, instructions);
+    printf("Part 2: Value of wire %s after override of wire b = %u.\n", target, target_value);
+
+    empty_and_free_instruction_list(instructions);
 
     return 0;
 }
@@ -68,118 +93,124 @@ char* chomp(char *s) {
     return s;
 }
 
-void handle_instruction (FILE *in_file, WIRE_LIST_T **head) {
-    char buffer[BUFFER_SIZE] = {0}, *ptr_buffer = &buffer[0];
-    if (fgets(buffer, sizeof(buffer), in_file)) {
-        chomp(buffer);
-        unsigned short val_1 = 0, val_2 = 0;
-        INSTRUCTION_TYPES inst_type;
-        if (isdigit(*ptr_buffer)) {
-            inst_type = i_assign;
-            sscanf(ptr_buffer, "%hu", &val_1);
-        } else {
-            if (strncmp(buffer, "NOT", 3) == 0) {
-                inst_type = i_not;
-            } else {
-                char tmp_label[LABEL_SIZE];
-                sscanf(buffer, "%s", tmp_label);
-                WIRE_LIST_T *tmp_wl = NULL;
-                while ((tmp_wl = find_wire(tmp_label, *head)) == NULL) {
-                    handle_instruction(in_file, head);
-                }
-                val_1 = get_wire_val(tmp_label, tmp_wl);
-            }
+size_t count_spaces (const char *s) {
+    size_t count = 0;
+    while (*s) {
+        if (*s == ' ') {
+            count++;
         }
+        s++;
+    }
+    return count;
+}
 
-        if (inst_type != i_not) {
-            ptr_buffer = strchr(ptr_buffer, ' ');
-            ptr_buffer++;
-        }
+instruction_list_t* add_new_instruction (instruction_list_t *l, const char *s) {
+    instruction_list_t *new_instruction = (instruction_list_t *) calloc (1, sizeof(instruction_list_t));
+    new_instruction->next = l;
+    new_instruction->instruction.val_available = false; // should be default value
 
-        switch(*ptr_buffer) {
-            case '-': // assignation
-                ptr_buffer = strchr(ptr_buffer, ' ');
-                ptr_buffer++;
-                add_wire_to_list(ptr_buffer, val_1, head);
-                printf("Assigned value of %d to label %s.\n", val_1, ptr_buffer);
-                return;
-            case 'A': // AND
-                inst_type = i_and;
-                break;
-            case 'O': // OR
-                inst_type = i_or;
-                break;
-            case 'L': // LSHIFT
-                inst_type = i_lshift;
-                break;
-            case 'R': // RSHIFT
-                inst_type = i_rshift;
-                break;
-            default: ; // NOT, do nothing
-        }
+    char *ptr_target = strrchr(s, ' ');
+    ptr_target++;
+    strcpy(new_instruction->instruction.target_label, ptr_target);
 
-        ptr_buffer = strchr(ptr_buffer, ' ');
-        ptr_buffer++;
-
-        if (isdigit(*ptr_buffer)) {
-            sscanf(ptr_buffer, "%hu", &val_2);
-        } else {
-            char tmp_label[LABEL_SIZE];
-            sscanf(ptr_buffer, "%s", tmp_label);
-            WIRE_LIST_T *tmp_wl = NULL;
-            while ((tmp_wl = find_wire(tmp_label, *head)) == NULL) {
-                handle_instruction(in_file, head);
-            }
-            val_2 = get_wire_val(tmp_label, tmp_wl);
-        }
-
-        ptr_buffer++;
-        ptr_buffer = strchr(ptr_buffer, ' ');
-        ptr_buffer++;
-        ptr_buffer = strchr(ptr_buffer, ' ');
-        ptr_buffer++;
-
-        switch(inst_type) {
-            case i_and:
-                add_wire_to_list(ptr_buffer, val_1 & val_2, head);
-                break;
-            case i_or:
-                add_wire_to_list(ptr_buffer, val_1 | val_2, head);
-                break;
-            case i_lshift:
-                add_wire_to_list(ptr_buffer, val_1 << val_2, head);
-                break;
-            case i_rshift:
-                add_wire_to_list(ptr_buffer, val_1 >> val_2, head);
-                break;
-            case i_not:
-                add_wire_to_list(ptr_buffer, val_2^0xFFFF, head);
-                break;
-            case i_assign: // do nothing, case already handled earlier
-                break;
+    if (count_spaces(s) == 2) {
+        new_instruction->instruction.i_type = i_assign;
+        sscanf(s, "%s", new_instruction->instruction.right_label);
+    } else if (strncmp(s, "NOT", strlen("NOT")) == 0) {
+        new_instruction->instruction.i_type = i_not; // alternatively: if (count_spaces(s) == 3) {
+        char *ptr_s = strchr(s, ' ');
+        ptr_s++;
+        sscanf(ptr_s, "%s", new_instruction->instruction.right_label);
+    } else {
+        char i_type_buffer[BUFFER_SIZE];
+        sscanf(s, "%s %s %s", new_instruction->instruction.left_label, i_type_buffer, new_instruction->instruction.right_label);
+        if (strcmp(i_type_buffer, "OR") == 0) {
+            new_instruction->instruction.i_type = i_or;
+        } else if (strcmp(i_type_buffer, "AND") == 0) {
+            new_instruction->instruction.i_type = i_and;
+        } else if (strcmp(i_type_buffer, "LSHIFT") == 0) {
+            new_instruction->instruction.i_type = i_lshift;
+        } else /* if (strcmp(i_type_buffer, "RSHIFT") == 0) */ {
+            new_instruction->instruction.i_type = i_rshift;
         }
     }
+
+    return new_instruction;
 }
 
-void add_wire_to_list (const char *s, unsigned short val, WIRE_LIST_T **head) {
-    WIRE_LIST_T *new_wl = (WIRE_LIST_T *) malloc (sizeof(WIRE_LIST_T));
-    strcpy(new_wl->wire.label, s);
-    new_wl->wire.val = val;
-    new_wl->next = *head;
-    *head = new_wl;
-}
-
-WIRE_LIST_T* find_wire (const char *s, WIRE_LIST_T *head) {
-    while (head) {
-        if (strcmp(head->wire.label, s) == 0) {
-            return head;
+instruction_t* get_target_instruction (instruction_list_t *l, const char *s) {
+    while (l) {
+        if (strcmp(l->instruction.target_label, s) == 0) {
+            return &(l->instruction);
         }
-        head = head->next;
+        l = l->next;
     }
     return NULL;
 }
 
-unsigned short get_wire_val (const char *s, WIRE_LIST_T *head) {
-    WIRE_LIST_T *wire = find_wire(s, head);
-    return wire == NULL ? 0 : wire->wire.val;
+void override_instruction (instruction_list_t *l, const char *s, instruction_types it, const char *ll, const char *rl) {
+    instruction_t *instr = get_target_instruction(l, s);
+    instr->i_type = it;
+    strcpy(instr->left_label, ll);
+    strcpy(instr->right_label, rl);
+}
+
+void clear_values (instruction_list_t *l) {
+    while (l) {
+        l->instruction.val_available = false;
+        l = l->next;
+    }
+}
+
+void empty_and_free_instruction_list (instruction_list_t *l) {
+    while (l) {
+        instruction_list_t *tmp_l = l;
+        l = l->next;
+        free(tmp_l);
+    }
+}
+
+unsigned short get_value_of_target (const char *t, instruction_list_t *il) {
+    unsigned int res_val = 0;
+    instruction_t *instr = get_target_instruction(il, t);
+    if (instr) {
+        if (instr->val_available) {
+            return instr->val;
+        }
+
+        unsigned short left_val = 0, right_val = 0;
+
+        if (instr->i_type != i_not && instr->i_type != i_assign) {
+            if (isdigit(instr->left_label[0])) {
+                left_val = (unsigned short) strtol(instr->left_label, NULL, 10);
+            } else {
+                left_val = get_value_of_target(instr->left_label, il);
+            }
+        }
+        if (isdigit(instr->right_label[0])) {
+            right_val = (unsigned short) strtol(instr->right_label, NULL, 10);
+        } else {
+            right_val = get_value_of_target(instr->right_label, il);
+        }
+
+        switch(instr->i_type) {
+            case i_or : res_val = (left_val | right_val);
+                break;
+            case i_and : res_val = (left_val & right_val);
+                break;
+            case i_lshift : res_val = (left_val << right_val);
+                break;
+            case i_rshift : res_val = (left_val >> right_val);
+                break;
+            case i_not : res_val = (right_val^0xFFFF);
+                break;
+            case i_assign : res_val = right_val;
+                break;
+        }
+
+        instr->val_available = true;
+        instr->val = res_val;
+    }
+
+    return res_val;
 }
